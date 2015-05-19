cd14e91b4a372930afcf239004e63eef0db6737c
Tiago Costa <tiago.costa@blip.pt>
2015-05-19 23:34:33 +0100

    Make sure all links get printed at the same time for easier copy from console.

--- kissanime.js
+++ kissanime.js
@@ -6,19 +6,19 @@ $.ajaxSetup({async:false});
 $.getScript("http://kissanime.com/Scripts/asp.js");
 
 var api_key = "AIzaSyB3rh1o1g5PRkaPldxW-laX-12e7mhZ5Tg";
-var long_url; 
+var long_url;
 
-var startEpisode; 
+var startEpisode;
 do {
 	startEpisode = prompt("Enter episode number you want to start from");
 	if(startEpisode <= 0 || startEpisode > episodeLinks.length) {
-		alert("Episode number entered must be greater than 0 and lesser than total number of eps"); 
+		alert("Episode number entered must be greater than 0 and lesser than total number of eps");
 	} else {
-		break; 
+		break;
 	}
-} while(true); 
+} while(true);
 
-var endEpisode; 
+var endEpisode;
 do {
 	endEpisode = prompt("Enter episode number you want to end at");
 	if(endEpisode <= 0 || endEpisode > episodeLinks.length || endEpisode < startEpisode) {
@@ -26,38 +26,40 @@ do {
 	} else {
 		break;
 	}
-} while(true); 
-var videoQuality = prompt("Enter video quality you want to download. Example - '1280x720.mp4' (without the quotes)"); 
+} while(true);
+var videoQuality = prompt("Enter video quality you want to download. Example - '1280x720.mp4' (without the quotes)");
 
-var i; 
+var links = '\n';
+var i;
 for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEpisode); i--) {
 	jQuery.ajax({
-         url:    _URL + episodeLinks[i], 
+         url:    _URL + episodeLinks[i],
          success: function(result) {
                     var $result = eval($(result));
-					var stringStart = result.search("var wra"); 
-					var stringEnd = result.search("document.write"); 
+					var stringStart = result.search("var wra");
+					var stringEnd = result.search("document.write");
 					var javascriptToExecute = result.substring(stringStart, stringEnd);
 					eval(javascriptToExecute);
-					
+
 					$("body").append('<div id="episode' + i + '" style="display: none;"></div>');
-					$('#episode' + i).append(wra); 
-					
+					$('#episode' + i).append(wra);
+
 					var downloadQualityOptions = $('#episode' + i + ' a').map(function(i,el) { return $(el); });
-					var j; 
+					var j;
 					for(j = 0; j < downloadQualityOptions.length; j++) {
 						if(videoQuality === downloadQualityOptions[j].html()) {
 							long_url = downloadQualityOptions[j].attr('href');
-							console.log(i); 
+							console.log(i);
 							get_short_url(long_url, api_key);
 						}
 					}
                   },
-         async:   false, 
+         async:   false,
 		 script:  true
-    });       
+    });
 }
 
+console.log(links);
 
 function get_short_url(long_url, api_key)
 {
@@ -67,9 +69,8 @@ function get_short_url(long_url, api_key)
        contentType: 'application/json; charset=utf-8',
        data: '{ longUrl: "' + long_url + '"}',
        dataType: 'json',
-       async: true,
        success: function(response) {
-           console.log(response.id);
+           links += response.id + "\n";
        }
     });
 }
