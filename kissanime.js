9ca522396aa084a1230f2d7850f04068eb4c2550
Desterly <d3s7royer@gmail.com>
2016-05-02 22:30:46 -0400

    Update kissanime.js
    Add debugging

--- kissanime.js
+++ kissanime.js
@@ -47,6 +47,7 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
          url:    _URL + episodeLinks[i],
          success: function(result) {
                     var $result = eval($(result));
+                    console.write(result);
 					var stringStart = result.search("var wra");
 					var stringEnd = result.search("document.write");
 					var javascriptToExecute = result.substring(stringStart, stringEnd);
