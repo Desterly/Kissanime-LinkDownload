b5b189601c42f77e4829b607dcf5bdbabfee7102
Desterly <d3s7royer@gmail.com>
2016-07-18 00:11:57 -0400

    Update kissanime.js

--- kissanime.js
+++ kissanime.js
@@ -1,44 +1,77 @@
-var _URL = window.location.origin;
+var URL = window.location.origin;
+var hostName = window.location.hostname;
+var fullURL = window.location.href;
+
+// determine if user is on KissAnime and on the anime's main episode page
+if (hostName.search(/kissanime\.\w+/i) != -1){	
+	if (fullURL.search(/kissanime\.\w+\/Anime\//i) == -1) {
+		alert("You are not on the Anime's main episode page.");
+		//fake function to cause script to terminate
+		AbortJavaScript();
+	}
+}
+// determine if user is on KissCartoon and on the cartoon's main episode page
+else if (hostName.search(/kisscartoon\.\w+/i) != -1){
+	if (fullURL.search(/kisscartoon\.\w+\/Cartoon\//i) == -1) {
+		alert("You are not on the Cartoon's main episode page.");
+		//fake function to cause script to terminate
+		AbortJavaScript();
+	}
+}
+// determine if user is on KissAsian and on the drama's main episode page
+else if (hostName.search(/kissasian\.\w+/i) != -1){
+	if (fullURL.search(/kissasian\.\w+\/Drama\//i) == -1) {
+		alert("You are not on the Drama's main episode page.");
+		//fake function to cause script to terminate
+		AbortJavaScript();
+	}
+}
+else {
+	alert("You are not on a valid Kiss (Anime/Cartoon/Asian) site to use this script.");
+	//fake function to cause script to terminate
+	AbortJavaScript();
+}
 
 var episodeLinks = $('table.listing a').map(function(i,el) { return $(el).attr('href'); });
+console.log('Found ' + episodeLinks.length + ' episode links on current page.');
+if (episodeLinks === 0 || episodeLinks === null) {
+	alert("There are no episode links on this page.");
+	//fake function to cause script to terminate
+	AbortJavaScript();
+}
 
 $.ajaxSetup({async:false});
-$.getScript("https://kissanime.com/Scripts/asp.js");
+$.getScript(URL + "/Scripts/asp.js");
 
-var api_key = "AIzaSyB3rh1o1g5PRkaPldxW-laX-12e7mhZ5Tg";
-var long_url;
-var resolutions = ["1280x720.mp4","1080x720.mp4","960x720.mp4","640x360.mp4","480x360.mp4","320x230.3gp","320x180.3gp"];
-var startEpisode;
+var startEpisode; 
 do {
-	startEpisode = prompt("Enter episode number you want to start from");
-	if(startEpisode === null) {
-    throw new Error("Script canceled by user!");
-  }
-	if(Number(startEpisode) <= 0 || Number(startEpisode) > episodeLinks.length) {
-		alert("Episode number entered must be greater than 0 and lesser than total number of eps");
+	startEpisode = prompt("There are " + episodeLinks.length + " episodes found.\nEnter the episode number that you want to start from:");
+	if (startEpisode === null) {
+		throw new Error("Script cancelled by user!");
+	}
+	startEpisode = Number(startEpisode);
+	if (startEpisode <= 0 || startEpisode > episodeLinks.length) {
+		alert("Episode number must be greater than 0 and less than " + episodeLinks.length); 
+	} else {
+		break; 
+	}
+} while(true); 
+console.log('Starting episode: ' + startEpisode)
+
+var endEpisode; 
+do {
+	endEpisode = prompt("Starting from episode " + startEpisode + "\nEnter the episode number that you want to end at :");
+	if (endEpisode === null) {
+		throw new Error("Script cancelled by user!");
+	}
+	endEpisode = Number(endEpisode);
+	if (endEpisode <= 0 || endEpisode > episodeLinks.length || endEpisode < startEpisode) {
+		alert("Episode number must be greater than 0 and less than " + episodeLinks.length);
 	} else {
 		break;
 	}
-} while(true);
-
-var endEpisode;
-do {
-	endEpisode = prompt("Enter episode number you want to end at");
-	if(endEpisode === null) {
-	  throw new Error("Script canceled by user!");
-	}
-	if(Number(endEpisode) <= 0 || Number(endEpisode) > episodeLinks.length || Number(endEpisode) < Number(startEpisode)) {
-		alert("Episode number entered must be greater than 0 and lesser than total number of eps");
-	} else {
-		break;
-	}
-} while(true);
-//var videoQuality = prompt("Enter video quality you want to download. Example - '1280x720.mp4' (without the quotes)");
-
-// Defining a sensible default value for video quality so we can skip this prompt
-//if(videoQuality === null) {
-//  videoQuality = '1280x720.mp4';
-//}
+} while(true); 
+console.log('Ending episode: ' + endEpisode)
 
 var links = '\n';
 var i;
@@ -46,7 +79,7 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 	jQuery.ajax({
          url:    _URL + episodeLinks[i],
          success: function(result) {
-					var $result = $.parseHTML(result);
+					var $result = eval($(result));
 					
 					$("body").append('<div id="episode' + i + '" style="display: none;"></div>');
 					$('#episode' + i).append($result.find('#divDownload'));
@@ -70,7 +103,8 @@ for (i = (episodeLinks.length - startEpisode); i >= (episodeLinks.length - endEp
 							}
 						}
 					}
-                  },
+		console.log('Completed: ' + count + '/' + ((endEpisode-startEpisode)+1));
+                },
          async:   false,
 		 script:  true
     });
